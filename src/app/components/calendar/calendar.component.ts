873104b1af6d443af62ace8c5684d48426acd607
zhantyzgz <zhantyzgz@gmail.com>
2017-05-26 15:03:44 +0200

    kinda bad code with more repetition than necessary, but now you can select a year too!

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,5 +1,6 @@
 //TODO:
-//make monthSelector be able to change between years
+//optimize
+//make buttons work for years
 import { Component, AfterViewChecked , Input } from '@angular/core';
 import { Event } from '../../models/event.model'
 import { Subject } from 'rxjs/Subject';
@@ -29,6 +30,12 @@ interface Month {
   year: number;
 }
 
+interface Year {
+  year: number,
+  months: Month[],
+  numberOfEvents: number
+}
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
@@ -41,7 +48,8 @@ export class CalendarComponent implements AfterViewChecked  {
   viewDate: Date = new Date();
   refresh: Subject<any> = new Subject();
   monthSelectorActivated = false;
-  months: Array<Month[]> = [];
+  yearSelectorActivated = false;
+  yearsAndMonths: Array<Year> = [];
   currentYear: string;
 
   constructor() {
@@ -50,9 +58,12 @@ export class CalendarComponent implements AfterViewChecked  {
   }
 
   ngAfterViewChecked() {
-    for(let year in this.months){
-      for(let month of this.months[year]){
-        month.numberOfEvents = 0;
+    for(let year of this.yearsAndMonths){
+      if(year){
+        for(let month of year.months){
+          month.numberOfEvents = 0;
+        }
+      year.numberOfEvents = 0;
       }
     }
     this.calendarEvents = [];
@@ -67,17 +78,23 @@ export class CalendarComponent implements AfterViewChecked  {
       }
       let day = event.schedule[0].start;
       let year = day.getFullYear()
-      if(!(year in this.months)){
-        this.initializeYear(year);
-      }
-      this.months[year][day.getMonth()].numberOfEvents += 1;
+      this.initializeYear(year);
+      this.yearsAndMonths[year].months[day.getMonth()].numberOfEvents += 1;
+      this.yearsAndMonths[year].numberOfEvents += 1
     }
 
   }
 
   initializeYear(year: number|string){
+    if(year in this.yearsAndMonths){
+      return undefined
+    }
     year = year.toString()
-    this.months[year] = [];
+    this.yearsAndMonths[year] = {
+      year: Number(year),
+      months: [],
+      numberOfEvents: 0
+    }
     for(let index in monthNames){
       let month = {
         name: monthNames[index],
@@ -85,7 +102,27 @@ export class CalendarComponent implements AfterViewChecked  {
         year: year,
         date: new Date(Number(year), Number(index), 1, 0, 0, 0)
       }
-      this.months[year].push(month)
+      this.yearsAndMonths[year].months.push(month)
     }
+  }
+
+  yearRange(year : number|string){
+    year = Number(year);
+    let list = [];
+    for(let i = year-4; i <= year+4; i++){
+      if(!(i.toString() in this.yearsAndMonths)){
+        this.initializeYear(i.toString())
+      }
+      list.push(this.yearsAndMonths[i.toString()]);
+    }
+    return list;
+  }
+  previousYear(){
+    this.currentYear = (Number(this.currentYear)-1).toString();
+    this.initializeYear(this.currentYear);
+  }
+  nextYear(){
+    this.currentYear = (Number(this.currentYear)+1).toString();
+    this.initializeYear(this.currentYear);
   }
 }
